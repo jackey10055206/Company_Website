a501605512c520f00de5c0edfe712b3533735625
jackey10055206 <jackey10055206@gmail.com>
2026-02-13 14:16:25 +0800

    chore(brand): bump logo svg filename to bust cache

--- my-portfolio-frontend/src/app/(site)/layout.tsx
+++ my-portfolio-frontend/src/app/(site)/layout.tsx
@@ -18,7 +18,7 @@ export default function SiteLayout({ children }: { children: React.ReactNode }) 
           {/* Use <img> for SVG to avoid next/image SVG restrictions/optimization */}
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
-            src="/brand/logo_1.svg"
+            src="/brand/logo_2.svg"
             alt="公司 Logo"
             className="h-14 w-auto md:h-24"
           />
